f39c8f7a48cd3162211e3a62eeec4b70ebfe1e4c
Daniel Isenmann <daniel@archlinux.org>
2016-04-26 22:03:36 +0200

    Add a small demo application to show how to authenticate the user against the 23 service and how to get the authenticaton token for all other operations.

---
+++ DemoApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwentyThreeNet;
+
+namespace DemoApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwentyThree twentyThree = new TwentyThree("DEMOKEY","DEMOSHARED");
+
+            // Getting the frob and calculate the URL
+            string frob = twentyThree.AuthGetFrob();
+            string url = twentyThree.AuthCalcUrl(frob, AuthLevel.Delete);
+
+            // Pointing the user to the URL with its standard browser
+            Process p = Process.Start(url);
+            p.WaitForExit();
+
+            // Get the authentication token to access 23, remember this for all other methods
+            Auth auth = twentyThree.AuthGetToken(frob);
+            string token = auth.Token;
+        }
+    }
+}
